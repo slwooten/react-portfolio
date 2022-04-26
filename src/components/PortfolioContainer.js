70dc36df86e212e6d73f89f0aa4ec68508d7c9fd
Seth Wooten <slwooten14@gmail.com>
2022-04-26 18:59:09 -0400

    added to portfolioContainer.js, conditional for rendering current page

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import AboutMe from './pages/AboutMe';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('AboutMe');
+
+  const renderPage = () => {
+    if (currentPage === 'AboutMe') {
+      return <AboutMe />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+    return <Resume />;
+  }
+}
