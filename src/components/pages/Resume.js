f2ef93786575b94c7481437d549fe92f13ea838f
Seth Wooten <slwooten14@gmail.com>
2022-04-27 18:57:08 -0400

    finished resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,22 +2,23 @@ import React from 'react';
 
 export default function Resume() {
   return (
-    <div>
-      <h1>Resume</h1>
-      <div>
-        <a href='https://drive.google.com/file/d/1U6oNME2HAkzBCuy5g7TXx4jdTSG0yEga/view?usp=sharing'>View My Resume</a>
-        <h2>Front End Proficiencies</h2>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>Bootstrap</li>
-        </ul>
-        <h2>Front End Proficiencies</h2>
-        <ul>
-          <li>Node</li>
-          <li>Express</li>
-          <li>Restful API</li>
-        </ul>
+    <div class='d-flex justify-content-center align-items-center m-3'>
+      <div class='card m-3 w-50'>
+        <h1 class='card-title p-3'>Resume</h1>
+          <a class='p-3' href='https://drive.google.com/file/d/1U6oNME2HAkzBCuy5g7TXx4jdTSG0yEga/view?usp=sharing' target='new'>View My Resume ></a>
+        <div class='card-body'>
+          <h2>Proficiencies</h2>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Bootstrap</li>
+            <li>React</li>
+            <li>Node</li>
+            <li>Express</li>
+            <li>MongDB</li>
+            <li>SQL</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
